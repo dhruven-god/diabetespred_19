b694c707c81a3ef3cfd3760d5dbf584da8f2fccc
Dhruven <52949581+dhruven-god@users.noreply.github.com>
2020-11-11 22:14:18 +0530

    Add files via upload

--- diabetespred/settings.py
+++ diabetespred/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'myapp.apps.MyappConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 STATICFILES_DIRS = [
